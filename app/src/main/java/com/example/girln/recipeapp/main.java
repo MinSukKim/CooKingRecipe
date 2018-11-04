ed211d68cc5d8ac6bada53aa1327ccbe6b3cf4d4
girln <minsuk.kim.001@student.uni.lu>
2018-11-04 12:40:08 +0100

    search

--- app/src/main/java/com/example/girln/recipeapp/main.java
+++ app/src/main/java/com/example/girln/recipeapp/main.java
@@ -30,14 +30,6 @@ public class main extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -55,8 +47,6 @@ public class main extends AppCompatActivity
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user != null) {
             for (UserInfo profile : user.getProviderData()) {
-
-                String uid = profile.getUid();
 
                 String name = profile.getDisplayName();
                 String email = profile.getEmail();
@@ -92,9 +82,9 @@ public class main extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
